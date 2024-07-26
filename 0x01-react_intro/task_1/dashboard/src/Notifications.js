6c7c49718969270ee612a02195e8841fbb7efb9f
DianaWangui <dianawanguikt048@gmail.com>
2024-07-26 14:16:24 +0300

    update the notification css class

--- 0x01-react_intro/task_1/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -1,6 +1,8 @@
+import React from 'react';
+import './Notifications.css';
 export default function myNotifications() {
   return (
-    <div>
+    <div className="Notifications">
       <p>
         Here is the list of notifications
       </p>
--- 0x01-react_intro/task_1/dashboard/src/utils.js
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -1,12 +1,12 @@
 function getFullYear() {
   return (
-    new Date().getFullYear()
+    new Date().getFullYear();
   )
 }
 
 function getFooterCopy(isIndex) {
   if (isIndex) {
-    return 'Holberton School'
+    return 'Holberton School';
   }
-  return 'Holberton School main dashboard'
+  return 'Holberton School main dashboard';
 }
