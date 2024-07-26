cfd6879fc3817494dd7ba51b256c165e9e68e1cd
DianaWangui <dianawanguikt048@gmail.com>
2024-07-26 14:26:08 +0300

    task 2

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,42 @@
+import Holberton_logo from './Holberton_logo.jpg';
+import './App.css'
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <header className="App-header">
+        <img src={Holberton_logo} className="App-logo" alt="logo" />
+        <h1>
+          School dashboard
+        </h1>
+      </header>
+    </div>
+
+    <div>
+      <body className="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+        <label for="email">Email</label>
+        <input type="email" id="email" />
+        <label for="password">Password</label>
+        <input type="password" id="password" />
+        <button>OK</button>
+      </body>
+    </div>
+
+    <div className="App-footer">
+      <footer>
+        <p>
+        Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </footer>
+    </div>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Notifications.css';
+export default function myNotifications() {
+  return (
+    <div className="Notifications">
+      <p>
+        Here is the list of notifications
+      </p>
+    </div>
+  )
+}
