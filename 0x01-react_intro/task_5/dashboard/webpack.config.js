b86e583335b0267b3cf586ea2ed485738a912ba1
DianaWangui <dianawanguikt048@gmail.com>
2024-07-30 15:08:18 +0300

    add dashboard

---
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['image-webpack-loader']
+            }
+        ]
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        hot: true,
+    }
+};
